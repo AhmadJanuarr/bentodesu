d2b0dac385d46096364ecf2109baef7023637738
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-02 15:31:03 +0700

    Fix typo in layout class name for main element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
     <html lang="id">
       <body className={`antialiased relative`}>
         <Header />
-        <main className="dekstop:w-[80%] laptop:w-[90%] w-full mx-auto px-5">{children}</main>
+        <main className="dekstop:w-[80%] laptop:w-[90%] w-full mx-auto px-5 relaitve">{children}</main>
         <Footer />
       </body>
     </html>
